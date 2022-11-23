de730543ab26b1cf70d34e5cf012a70691eb16dd
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-23 10:41:44 -0300

    feat: criação do arquivo styles para armazenar os estilos

---
+++ src/theme/Styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const StyledPage = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const StyledP = styled.p`
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 23px;
+    margin: 10px 0;
+`;
+
+export const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    input {
+        width: 320px;
+        height: 60px;
+        border: 1px solid;
+        border-radius: 5px;
+        margin: 10px 0;
+        text-indent: 5px;
+    }
+
+    button {
+        width: 320 px;
+        height: 60px;
+        background-color: antiquewhite;
+        border-radius: 5px;
+        border: 1px solid;
+    }
+`;
