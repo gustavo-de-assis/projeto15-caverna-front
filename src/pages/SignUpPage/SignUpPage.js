ba9ecde28d9df7333d1f46e9ece27469a0fd2592
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-23 11:11:55 -0300

    feat: Pág cadastro [mock]

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,10 +1,54 @@
+import { useState } from "react"
+import { StyledPage, StyledForm, StyledP } from "../../theme/Styles"
 
 export default function SignUpPage() {
+
+    const [userInfo, setUserInfo] = useState({name:"", email:"", password: ""})
     
-    
+    function formHandler(e){
+        const {name, value} = e.target;
+        setUserInfo({...userInfo, [name]:value})
+    }
+
+    function signUpUser(e){
+        e.preventDefault();
+        console.log(userInfo);
+    }
+
     return (
-        <>
-        </>
+        <StyledPage>
+            <StyledP>Sign-Up!</StyledP>
+            <StyledForm onSubmit={signUpUser}>
+                <input 
+                    name="name"
+                    type="text"
+                    value={userInfo.name}
+                    placeholder="Name"
+                    onChange={formHandler}
+                    required
+                />
+                <input
+                    name="email"
+                    type="email"
+                    value={userInfo.email}
+                    placeholder="Email"
+                    onChange={formHandler}
+                    required
+                />
+                <input
+                    name="password"
+                    type="password"
+                    value={userInfo.password}
+                    placeholder="Password"
+                    onChange={formHandler}
+                    required
+                />
+                <button type="submit">
+                    Continue
+                </button>
+            </StyledForm>
+
+        </StyledPage>
     )
 
 }
