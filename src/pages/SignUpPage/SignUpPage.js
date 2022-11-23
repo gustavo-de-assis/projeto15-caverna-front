ed9352ae015eabdba840c4f28803f8d98e818059
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-11-23 20:33:13 -0300

    feat: funcao de cadastro no bd

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,9 +1,13 @@
+import axios from "axios";
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 import { StyledPage, StyledForm, StyledP } from "../../theme/Styles"
 
 export default function SignUpPage() {
 
     const [userInfo, setUserInfo] = useState({name:"", email:"", password: ""})
+    const navigate = useNavigate();
+
     
     function formHandler(e){
         const {name, value} = e.target;
@@ -12,7 +16,20 @@ export default function SignUpPage() {
 
     function signUpUser(e){
         e.preventDefault();
-        console.log(userInfo);
+
+        //Gathering user info
+        const newUser = {...userInfo};
+        
+        //Connecting to API
+        const URL = 'http://localhost:5000/signUp';
+
+        axios.post(URL, newUser).then((ans)=>{
+            console.log("Cadastro realizado!", ans.data);
+            navigate("/");
+        }).catch((err)=>{
+            console.log(err.response.data)
+        })
+
     }
 
     return (
