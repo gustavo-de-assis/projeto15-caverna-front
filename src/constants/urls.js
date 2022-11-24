233901b2074703e469fd93e03742fb7bff77a4fd
¨higorpr¨ <higorpr@gmail.com>
2022-11-24 05:31:49 -0300

    feat: finish login flux - link login page to back-end and send user to main page (not created yet) on sucess / create button to allow user to go to sign-up page

---
+++ src/constants/urls.js
@@ -0,0 +1,3 @@
+export const loginUrl = "http://localhost:5000/login";
+
+export const signUpUrl = "http://localhost:5000/signUp";
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import styled from 'styled-components';
+import {ProjectContext} from '../../constants/Context';
+
+
+export default function MainPage() {
+    
+    return(
+        <StyledPage>
+        <p>MainPage test</p>
+        </StyledPage>
+    )
+}
+
+const StyledPage = styled.div`
+
+`;
