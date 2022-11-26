459beb6ce8470c507fc821ebde9067af9be7a1e2
higorpr <higorpr@gmail.com>
2022-11-26 04:41:05 -0300

    feat:product page implemented

--- src/App.js
+++ src/App.js
@@ -7,51 +7,40 @@ import ProductPage from "./pages/ProductPage/ProductPage";
 import PurchasePage from "./pages/PurchasePage/PurchasePage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import MainPage from "./pages/MainPage/MainPage";
+import CartPage from "./pages/CartPage/CartPage";
 
 function App() {
-    // State to hold user information after login
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-        token: "",
-        cart: [],
-    });
+	// State to hold user information after login
+	const [user, setUser] = useState({});
 
-    const [targetProduct, setTargetProduct] = useState({
-        name: "",
-        image: "",
-        description: "",
-        price: "",
-    });
+	const [targetProduct, setTargetProduct] = useState({});
 
-    const [search, setSearch] = useState("");
+	const [search, setSearch] = useState("");
 
-    return (
-        <ProjectContext.Provider
-            value={{
-                user,
-                setUser,
-                targetProduct,
-                setTargetProduct,
-                search,
-                setSearch,
-            }}
-        >
-            <BrowserRouter>
-                <GlobalStyle />
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/signUp" element={<SignUpPage />} />
-                    <Route path="/main" element={<MainPage />} />
-                    <Route path="/product" element={<ProductPage />} />
-                    <Route
-                        path="/purchase/:productId"
-                        element={<PurchasePage />}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </ProjectContext.Provider>
-    );
+	return (
+		<ProjectContext.Provider
+			value={{
+				user,
+				setUser,
+				targetProduct,
+				setTargetProduct,
+				search,
+				setSearch,
+			}}
+		>
+			<BrowserRouter>
+				<GlobalStyle />
+				<Routes>
+					<Route path="/" element={<LoginPage />} />
+					<Route path="/signUp" element={<SignUpPage />} />
+					<Route path="/main" element={<MainPage />} />
+					<Route path="/product" element={<ProductPage />} />
+                    <Route path="/cart" element={<CartPage />} />
+					<Route path="/purchase" element={<PurchasePage />} />
+				</Routes>
+			</BrowserRouter>
+		</ProjectContext.Provider>
+	);
 }
 
 export default App;
